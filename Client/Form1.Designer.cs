a02d81b9878bebb2f39cc2d11213355cd932b7e7
szymo <szymo@DESKTOP-C954U3Q>
2026-01-29 13:13:02 +0100

    Dodaj pliki projektów.

---
+++ Client/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace Client
+{
+    partial class Klient
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Clean = new Button();
+            groupBox1 = new GroupBox();
+            PortP = new TextBox();
+            SetOptions = new Button();
+            IP = new TextBox();
+            label2 = new Label();
+            label1 = new Label();
+            groupBox2 = new GroupBox();
+            SendMessage = new Button();
+            Odpowiedź = new GroupBox();
+            FromServerMessage = new RichTextBox();
+            groupBox3 = new GroupBox();
+            ToServerMessage = new RichTextBox();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            Odpowiedź.SuspendLayout();
+            groupBox3.SuspendLayout();
+            SuspendLayout();
+            // 
+            // Clean
+            // 
+            Clean.Location = new Point(322, 559);
+            Clean.Name = "Clean";
+            Clean.Size = new Size(63, 35);
+            Clean.TabIndex = 0;
+            Clean.Text = "Wyczyść";
+            Clean.UseVisualStyleBackColor = true;
+            Clean.Click += Clean_Click;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(PortP);
+            groupBox1.Controls.Add(SetOptions);
+            groupBox1.Controls.Add(IP);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Location = new Point(12, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(391, 100);
+            groupBox1.TabIndex = 1;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Dane połączeniowe z serwerem";
+            // 
+            // PortP
+            // 
+            PortP.Location = new Point(80, 52);
+            PortP.Name = "PortP";
+            PortP.Size = new Size(236, 23);
+            PortP.TabIndex = 5;
+            // 
+            // SetOptions
+            // 
+            SetOptions.Location = new Point(322, 28);
+            SetOptions.Name = "SetOptions";
+            SetOptions.Size = new Size(63, 52);
+            SetOptions.TabIndex = 4;
+            SetOptions.Text = "Ustaw";
+            SetOptions.UseVisualStyleBackColor = true;
+            SetOptions.Click += SetOptions_Click;
+            // 
+            // IP
+            // 
+            IP.Location = new Point(80, 25);
+            IP.Name = "IP";
+            IP.Size = new Size(236, 23);
+            IP.TabIndex = 2;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(22, 55);
+            label2.Name = "label2";
+            label2.Size = new Size(36, 15);
+            label2.TabIndex = 1;
+            label2.Text = "PORT";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(22, 28);
+            label1.Name = "label1";
+            label1.Size = new Size(17, 15);
+            label1.TabIndex = 0;
+            label1.Text = "IP";
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(SendMessage);
+            groupBox2.Controls.Add(Odpowiedź);
+            groupBox2.Controls.Add(groupBox3);
+            groupBox2.Controls.Add(Clean);
+            groupBox2.Location = new Point(12, 118);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(391, 612);
+            groupBox2.TabIndex = 2;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Dialog";
+            // 
+            // SendMessage
+            // 
+            SendMessage.Location = new Point(253, 559);
+            SendMessage.Name = "SendMessage";
+            SendMessage.Size = new Size(63, 35);
+            SendMessage.TabIndex = 5;
+            SendMessage.Text = "Wyślij";
+            SendMessage.UseVisualStyleBackColor = true;
+            SendMessage.Click += SendMessage_Click;
+            // 
+            // Odpowiedź
+            // 
+            Odpowiedź.Controls.Add(FromServerMessage);
+            Odpowiedź.Location = new Point(22, 301);
+            Odpowiedź.Name = "Odpowiedź";
+            Odpowiedź.Size = new Size(363, 239);
+            Odpowiedź.TabIndex = 4;
+            Odpowiedź.TabStop = false;
+            Odpowiedź.Text = "Odpowiedź z Serwera";
+            // 
+            // FromServerMessage
+            // 
+            FromServerMessage.Location = new Point(6, 22);
+            FromServerMessage.Name = "FromServerMessage";
+            FromServerMessage.Size = new Size(351, 211);
+            FromServerMessage.TabIndex = 1;
+            FromServerMessage.Text = "";
+            // 
+            // groupBox3
+            // 
+            groupBox3.Controls.Add(ToServerMessage);
+            groupBox3.Location = new Point(22, 22);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(363, 260);
+            groupBox3.TabIndex = 3;
+            groupBox3.TabStop = false;
+            groupBox3.Text = "Wiadomość do wysłania";
+            // 
+            // ToServerMessage
+            // 
+            ToServerMessage.Location = new Point(12, 22);
+            ToServerMessage.Name = "ToServerMessage";
+            ToServerMessage.Size = new Size(345, 232);
+            ToServerMessage.TabIndex = 0;
+            ToServerMessage.Text = "";
+            // 
+            // Klient
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(423, 742);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Name = "Klient";
+            Text = "Klient TCP";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            Odpowiedź.ResumeLayout(false);
+            groupBox3.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button Clean;
+        private GroupBox groupBox1;
+        private TextBox IP;
+        private Label label2;
+        private Label label1;
+        private GroupBox groupBox2;
+        private GroupBox Odpowiedź;
+        private GroupBox groupBox3;
+        private Button SetOptions;
+        private Button SendMessage;
+        private RichTextBox FromServerMessage;
+        private RichTextBox ToServerMessage;
+        private TextBox PortP;
+    }
+}
---
+++ Client/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System.Globalization;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+namespace Client
+{
+    public partial class Klient : Form
+    {
+        //Dane do ustawienia połącznia
+        IPAddress IPv4;
+        int Port;
+
+        //Szyfrowanie danych w ramce i określenie wielkości ramki
+        public const byte XOR_KEY = 0xAA;
+        public const int FRAME_SIZE = 16;
+
+        public Klient()
+        {
+            InitializeComponent();
+        }
+
+        //Ustawienie połączenia
+        private void SetOptions_Click(object sender, EventArgs e)
+        {
+            string ipText = IP.Text.Trim();
+            string portText = PortP.Text.Trim();
+            if (string.IsNullOrEmpty(ipText))
+            {
+                MessageBox.Show("Nie podano adresu IP!", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (string.IsNullOrEmpty(portText))
+            {
+                MessageBox.Show("Nie podano portu!", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (!IPAddress.TryParse(ipText, out _))
+            {
+                MessageBox.Show("Niepoprawny format adresu IP!", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (!int.TryParse(portText, out int port) || port < 1 || port > 65535)
+            {
+                MessageBox.Show("Port musi być liczbą z zakresu 1–65535!", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            IPv4 = IPAddress.Parse(ipText);
+            Port = int.Parse(portText);
+
+            MessageBox.Show("Ustawiono połączenie! \n \n Adres IP: " + ipText + "\n Port: " + portText, "Informacja", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        //Wyczyszczenie pól informacyjnych
+        private void Clean_Click(object sender, EventArgs e)
+        {
+            ToServerMessage.Text = "";
+            FromServerMessage.Text = "";
+        }
+
+        //Wysłanie wiadomości do serwera i jego odpowiedzi
+        private void SendMessage_Click(object sender, EventArgs e)
+        {
+            //Walidacja adresu IP i portu
+            if (IPv4 is null || PortP is null)
+            {
+                MessageBox.Show(
+                    "Sparametryzuj połączenie! \n \n Adres IP: " + IPv4 + "\n Port: " + PortP,
+                    "Informacja", MessageBoxButtons.OK, MessageBoxIcon.Information
+                );
+                return;
+            }
+
+            if (string.IsNullOrEmpty(ToServerMessage.Text))
+            {
+                MessageBox.Show(
+                    "Nie podano wiadomości!",
+                    "Operacja niedozwolona", MessageBoxButtons.OK, MessageBoxIcon.Stop
+                );
+                return;
+            }
+
+           
+            try
+            {
+                FromServerMessage.Text = "";
+
+                //ustawienie połączenia
+                using TcpClient client = new TcpClient();
+                client.Connect(IPv4, Port);
+
+                //Pozyskanie strumienia
+                using NetworkStream stream = client.GetStream();
+
+                // Przygotowanie wiadomości
+                string text = ToServerMessage.InvokeRequired
+                    ? (string)ToServerMessage.Invoke(new Func<string>(() => ToServerMessage.Text))
+                    : ToServerMessage.Text;
+
+                //Zakodowanie informacji do UTF8
+                byte[] data = Encoding.UTF8.GetBytes(text);
+                
+                //Podzielenie wiadomości na ramki
+                var frames = Split(data, 16); 
+
+                //Podzielenie ramki na kawałki
+                foreach (var chunk in frames)
+                {
+                    //Obliczenie sumy kontrolnej
+                    int checksum = Checksum(chunk);
+                    //Szyfrowanie kluczem XOR
+                    Xor(chunk);
+
+                    //Podzielenie ramki na części
+                    List<byte> frame = new List<byte>();
+                    frame.AddRange(BitConverter.GetBytes((ushort)0xCAFE));  //Magic Number
+                    frame.Add(1);                                           //wersja protokołu
+                    frame.Add(1);                                           //typ ramki
+                    frame.AddRange(BitConverter.GetBytes(chunk.Length));    //długość payload
+                    frame.AddRange(BitConverter.GetBytes(checksum));        //suma kontrolna
+                    frame.AddRange(chunk);                                  //zaszyfrowanie ramki
+
+                    stream.Write(frame.ToArray(), 0, frame.Count);          //wysłanie ramki do serwera
+                }
+
+                //Odebranie odpowiedzi od serwera
+                List<byte> allResponse = new List<byte>();
+                byte[] buffer = new byte[1024];
+                int bytesRead;
+
+                while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    allResponse.AddRange(buffer[..bytesRead]);
+
+                    // jeśli w buforze nie ma więcej danych, przerwij
+                    if (!stream.DataAvailable) break;
+                }
+
+                //Zdekodowanie całej odpowiedzi
+                byte[] respData = allResponse.ToArray();
+                Xor(respData);
+                string resp = Encoding.UTF8.GetString(respData);
+
+                //Odczytanie odpowiedzi
+                FromServerMessage.Invoke(() =>
+                {
+                    FromServerMessage.AppendText("Server: " + resp + Environment.NewLine);
+                });
+
+                client.Close();
+            }
+            catch (Exception ex)
+            {
+                //Zwrócenie błędu w przypadku np. utraty połączenia z serwerem
+                FromServerMessage.Invoke(() =>
+                {
+                    FromServerMessage.AppendText("ERROR: " + ex.Message + Environment.NewLine);
+                });
+            }
+        }
+
+        //Szyfrownaie z kluczem Xor do 
+        static void Xor(byte[] data)
+        {
+            for (int i = 0; i < data.Length; i++)
+                data[i] ^= XOR_KEY;
+        }
+        //Obliczneie sumy kontrolnej
+        static int Checksum(byte[] data)
+        {
+            int sum = 0;
+            foreach (byte b in data)
+                sum ^= b;
+            return sum;
+        }
+
+        //Podzielenie wiadomości na fragmęty
+        static List<byte[]> Split(byte[] data, int size)
+        {
+            List<byte[]> list = new List<byte[]>();
+            for (int i = 0; i < data.Length; i += size)
+            {
+                int len = Math.Min(size, data.Length - i);
+                byte[] chunk = new byte[len];
+                Array.Copy(data, i, chunk, 0, len);
+                list.Add(chunk);
+            }
+            return list;
+        }
+
+    }
+}
